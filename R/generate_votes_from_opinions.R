51ec74d4b69d10db5dee2c098c1a6c695c633afa
Johan Lindholm <48241545+jojolindholm@users.noreply.github.com>
2023-03-14 10:05:57 +0100

    added functions

---
+++ R/generate_votes_from_opinions.R
@@ -0,0 +1,33 @@
+# GENERATE VOTES FROM OPINIONS
+# v 0.99 per 28 February 2023
+# Johan Lindholm, Umeå University
+# ------------------------------------------------------------------------------
+# This function is part of the sehc Package and modify the SeHC Db
+# datasets to add commonly used variables. This function splits opinion into
+# votes.
+# ------------------------------------------------------------------------------
+
+
+generate_votes_from_opinions <- function(opinions) {
+
+  # check for opinion manipulation ---------------------------------------------
+  if (!"total_number_refs" %in% colnames(opinions)) {
+    i <- readline(prompt = "Do you want to add references variables? (y/n) ")
+    if (i == "y") {
+      opinions <- sehc::add_sources_to_opinions(opinions)
+    }
+  }
+
+  # split opinions into votes --------------------------------------------------
+  votes <- opinions |>
+    dplyr::filter(institution_class == "final" & opinion_class != "clerk") |>
+    tidyr::separate_rows(author_appointment_id, sep = ",") |>
+    dplyr::mutate(author_ref = as.integer(author_appointment_id == ref_appointment_id),
+                  .after = "author_appointment_id") |>
+    dplyr::select(-c("author_last_name", "ref_last_name", "ref_appointment_id"))
+  votes$author_ref[is.na(votes$author_ref)] <- 0
+
+  # return data ----------------------------------------------------------------
+  message("Opinions split into votes")
+  return(votes)
+}
