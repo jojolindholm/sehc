884e33f5261e1953d6a2b036b6226068e9927f6a
Johan Lindholm <48241545+jojolindholm@users.noreply.github.com>
2023-03-24 11:59:04 +0100

    method dimensions upodate

---
+++ R/add_method_dimensions_to_opinions.R
@@ -0,0 +1,58 @@
+# ADD METHOD DIMENSIONS TO OPINIONS
+# v 0.9.0 per 23 March 2023
+# Johan Lindholm, Umeå University
+#
+# This function is part of the sehc Package and combines data from difference
+# SeHC Db datasets. This function adds variables for the legal method used to
+# justify an opinion based on sources to Opinions.
+
+
+add_method_dimensions_to_opinions <- function(opinions) {
+
+  # check for opinions manipulation --------------------------------------------
+  if (!"total_refs_act" %in% colnames(opinions)) {
+      opinions <- sehc::add_sources_to_opinions(opinions)
+  }
+
+  # data manipulation ----------------------------------------------------------
+  hc_opinions <- filter(opinions, institution_class == "final" & opinion_class != "clerk")
+  refs_cols <- which(stringr::str_detect(colnames(hc_opinions), "total_refs_"))
+  names(refs_cols) <- stringr::str_remove(colnames(hc_opinions)[refs_cols], "total_refs_")
+
+  # extra-nationalism values ---------------------------------------------------
+  levels <- dplyr::select(sehc::method_dims, c("ref_type", "level")) |>
+    dplyr::mutate(level = stringr::str_replace(level, "extra_national", "1")) |>
+    dplyr::mutate(level = stringr::str_replace(level, "national", "0")) |>
+    dplyr::mutate(level = stringr::str_replace(level, "neither", "")) |>
+    dplyr::mutate(level = as.integer(level))
+  levels_ix <- match(names(refs_cols), levels$ref_type)
+
+  # extra-legislative values ---------------------------------------------------
+  authority <- dplyr::select(sehc::method_dims, c("ref_type", "authority")) |>
+    dplyr::mutate(authority = stringr::str_replace(authority, "extra_legislative", "1")) |>
+    dplyr::mutate(authority = stringr::str_replace(authority, "legislative", "0")) |>
+    dplyr::mutate(authority = stringr::str_replace(authority, "neither", "")) |>
+    dplyr::mutate(authority = as.integer(authority))
+  authority_ix <- match(names(refs_cols), authority$ref_type)
+
+  # main function --------------------------------------------------------------
+  calculate_opinion_method_dimensions <- function(id) {
+    opinion <- dplyr::filter(hc_opinions, opinion_id == id)
+    sources_extra_national_n <- sum((opinion[refs_cols] * levels$level[levels_ix]), na.rm = TRUE)
+    sources_extra_national_percent <- sources_extra_national_n / opinion$total_number_refs
+    sources_extra_legislative_n <- sum((opinion[refs_cols] * authority$authority[authority_ix]), na.rm = TRUE)
+    sources_extra_legislative_percent <- sources_extra_legislative_n / opinion$total_number_refs
+    data <- tibble::tibble(opinion_id = id,
+                           sources_extra_national_percent = sources_extra_national_percent,
+                           sources_extra_legislative_percent = sources_extra_legislative_percent)
+    return(data)
+  }
+
+  # main loop ------------------------------------------------------------------
+  message("Calculating method dimensions")
+  method_data <- pbapply::pblapply(hc_opinions$opinion_id, calculate_opinion_method_dimensions) |>
+    dplyr::bind_rows()
+  opinions <- dplyr::left_join(opinions, method_data, by = "opinion_id")
+  message("Method dimensions added to opinions")
+  return(opinions)
+}
--- example.R
+++ example.R
@@ -1,19 +1,19 @@
 # manipulate data --------------------------------------------------------------
 cases <- sehc::cases |>
   add_party_dummies_to_cases() |>
-  add_area_dummies_to_cases()
+  add_area_dummies_to_cases() |>
+  add_interests_involved_to_cases()
 
 opinions <- sehc::opinions |>
   add_cases_variables_to_opinions() |>
-  add_sources_to_opinions()
+  add_sources_to_opinions() |>
+  add_method_dimensions_to_opinions() |>
+  add_interest_impact_to_opinions()
 
 appointments <- sehc::appointments |>
   add_government_to_appointments() |>
   add_background_type_to_appointments()
 
-# combine data -----------------------------------------------------------------
-opinions <- opinions |>
-  add_cases_variables_to_opinions() |>
-  add_sources_to_opinions()
-
-votes <- generate_votes_from_opinions(opinions)
+# generate votes data ----- ----------------------------------------------------
+votes <- generate_votes_from_opinions(opinions) |>
+  add_appointments_to_votes()
